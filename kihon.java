3ba329659700e8287b5cee210dde7a13e1b789b3
菊地天聖 <kikuchitensei428@gmai.com>
2024-06-13 12:04:45 +0900

    javaを触り始めた

---
+++ kihon.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+
+public static void main(String[] args) {
+
+int x = 5;
+
+int y = 10;
+
+int z = x + y;
+
+String ans = "x+yは" + z;
+
+System.out.println(ans);
+
+}
+}
+
