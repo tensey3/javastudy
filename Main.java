3f1ad7743577551fcc3128a37b570d0c963335cc
tensey3 <kikuchitensei428@gmail.com>
2024-06-14 21:14:28 +0900

    if文に手を出す！

--- Main.java
+++ Main.java
@@ -5,7 +5,20 @@ public static void main(String[] args) {
 
 int rn = new java.util.Random().nextInt(4);
 
+short a = (short) rn;
+
+int  omikuzi = a;
+if (3 > a){
+    System.err.println("負け");
+    else {
+        System.err.println("勝ち");
+    }
+
+}
+
+
 System.out.println(rn);
 
 }
 }
+
