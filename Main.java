b75411530bb258bd493069cbb2867e3cbbd6669f
菊地天聖 <kikuchitensei428@gmai.com>
2024-06-18 03:09:42 +0900

    ちょっと条件変えた

--- Main.java
+++ Main.java
@@ -7,17 +7,14 @@ int rn = new java.util.Random().nextInt(4);
 
 short a = (short) rn;
 
-int  omikuzi = a;
-if (3 > a){
-    System.err.println("負け");}
-else {
-    System.err.println("勝ち");
-    }
+boolean tenki = true;
 
+if (tenki == true){
+    System.err.println("今日は晴れなので外で服を干します");
+}
+else{
+    System.err.println("雨なので今日は家で映画見ます");
 }
 
-
-
 }
-
-
+}
