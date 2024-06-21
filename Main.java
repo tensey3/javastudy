e72e5d75ce09e7fa3262cf9e0d5ca073959c7c38
tensey3 <kikuchitensei428@gmail.com>
2024-06-21 10:16:49 +0900

    特殊なループ

--- Main.java
+++ Main.java
@@ -6,8 +6,12 @@ public static void main(String[] args) {
 int rn = new java.util.Random().nextInt(4);
 
 for(int i = 0; i < 10; i++){
-    i += 1;
+    
     System.err.println(++i);
+}
+
+for (int j = 0; j < 10; j +=2) {
+    System.err.println(++j);
 }
 }
 }
