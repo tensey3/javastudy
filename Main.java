bda452eb1ee5e7b5cee0d0796df929d47a53b063
菊地天聖 <kikuchitensei428@gmai.com>
2024-06-21 10:21:05 +0900

    ランダム用いたループ

--- Main.java
+++ Main.java
@@ -5,13 +5,16 @@ public static void main(String[] args) {
 
 int rn = new java.util.Random().nextInt(4);
 
-for(int i = 0; i < 10; i++){
+// for(int i = 0; i < 10; i++){
     
-    System.err.println(++i);
-}
+//     System.err.println(++i);
+// }
 
-for (int j = 0; j < 10; j +=2) {
-    System.err.println(++j);
+// for (int j = 0; j < 10; j +=2) {
+//     System.err.println(++j);
+// }
+for (int p = 0; p < rn; p++) {
+    System.err.println("影分身の術！");
 }
 }
 }
