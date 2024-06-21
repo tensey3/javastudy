be33ec295b80490f8f3411b345da336fab77b653
菊地天聖 <kikuchitensei428@gmai.com>
2024-06-21 10:05:43 +0900

    ループ変数書いてみた

--- Main.java
+++ Main.java
@@ -5,16 +5,9 @@ public static void main(String[] args) {
 
 int rn = new java.util.Random().nextInt(4);
 
-short a = (short) rn;
-
-boolean tenki = true;
-
-if (tenki == true){
-    System.err.println("今日は晴れなので外で服を干します");
+for(int i = 0; i < 10; i++){
+    i += 1;
+    System.err.println(++i);
 }
-else{
-    System.err.println("雨なので今日は家で映画見ます");
-}
-
 }
 }
