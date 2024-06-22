d1dc13b5f35e9c8d73bb21f8a4bd6e1cd954cb61
tensey3 <kikuchitensei428@gmail.com>
2024-06-22 13:31:32 +0900

    　クラス作った

---
+++ account.java
@@ -0,0 +1,14 @@
+
+import java.security.PublicKey;
+
+public class account {
+    public static void main(String[] args) {
+    String accountNunber;
+
+    int balance;
+
+    account ac = new account();
+    
+    
+}
+}
