36f32e8db5d733e2333028f6ec9507a78ac7ceee
菊地天聖 <kikuchitensei428@gmail.com>
2024-06-26 23:59:59 +0900

    ifぶん

---
+++ IfStatement.java
@@ -0,0 +1,10 @@
+public class IfStatement {
+    public static void main(String[] args) {
+        int x = 20;
+        if (x > 10) {
+            System.out.println("xは10より大きい");
+        } else {
+            System.out.println("xは10以下");
+        }
+    }
+}
