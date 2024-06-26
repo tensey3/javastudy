4cfecdaf02006c2f35f37bc57f8a6f90b3a4ef76
菊地天聖 <kikuchitensei428@gmail.com>
2024-06-26 23:59:59 +0900

    型概念

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // 文字列を出力
-        System.out.println("Hello, World!");
-    }
-}
---
+++ Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args) {
+        // 整数型の変数
+        int myNumber = 10;
+        // 浮動小数点数型の変数
+        double myDouble = 5.99;
+        // 文字型の変数
+        char myChar = 'D';
+        // 文字列型の変数
+        String myString = "Hello";
+        // 論理型の変数
+        boolean myBoolean = true;
+        
+        // 変数の値を出力
+        System.out.println(myNumber);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myString);
+        System.out.println(myBoolean);
+    }
+}
