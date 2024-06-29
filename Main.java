a8bda3ac12c5759f033b85b0e0598c893d9ea72e
菊地天聖 <kikuchitensei428@gmai.com>
2024-06-29 12:12:20 +0900

    git@github.com:yufukui-m/vanxxx-server.git

--- Main.java
+++ Main.java
@@ -1,47 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int H = scanner.nextInt();
-        int W = scanner.nextInt();
-        scanner.nextLine(); 
-        
-        char[][] matrix = new char[H][W];
-        
-        for (int i = 0; i < H; i++) {
-            String line = scanner.nextLine();
-            matrix[i] = line.toCharArray();
-        }
-        
-        int maxArea = 0;
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < W; j++) {
-                if (matrix[i][j] == '.') {
-                   
-                    int minWidth = W;
-                    for (int k = i; k < H && matrix[k][j] == '.'; k++) {
-                        int width = 0;
-                        while (j + width < W && matrix[k][j + width] == '.') {
-                            width++;
-                        }
-                        if (width < minWidth) {
-                            minWidth = width;
-                        }
-                        int height = k - i + 1;
-                        int area = minWidth * height;
-                        if (area > maxArea) {
-                            maxArea = area;
-                        }
-                    }
-                }
+        for (int i = 1; i <= 100; i++) {
+            sb.append(i);
+            if (i < 100) {
+                sb.append(",");
             }
         }
-        
-        System.out.println(maxArea);
-        
-        scanner.close();
+        sb.append(".");
+
+        String s = sb.toString();
+        System.out.println("生成された文字列: " + s);
+
+        String[] stringArray = s.substring(0, s.length() - 1).split(",");
+
+        System.out.println("String 配列:");
+        }
     }
-}
