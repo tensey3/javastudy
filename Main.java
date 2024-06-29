c8227e300bc6f5e4d90db74e00132490b2fb07e1
tensey3 <kikuchitensei428@gmail.com>
2024-06-29 14:15:48 +0900

    りーたんの復讐

--- Main.java
+++ Main.java
@@ -1,15 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i <= 100; i++) {
-            sb.append(i);
-            if (i < 100) {
-                sb.append(",");
-            }
-        }
-
-        String s = sb.toString();
-        System.out.println(s);
-        }
+        Main main = new Main();
+        int result = main.add(5, 3,8); // addメソッドを呼び出し、5と3を渡す
+        System.out.println("結果は: " + result); // 結果は: 8 と表示される
     }
+
+    public int add(int a, int b,int v) {
+        return a + b; // 足し算の結果を返す
+    }
+}
