589303ccfaf1ef6d50829ac3b5c72cbf22a16083
tensey3 <kikuchitensei428@gmail.com>
2024-07-01 15:11:06 +0900

    　入力処理

---
+++ KeyEventDemo.java
@@ -0,0 +1,95 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class KeyEventDemo extends JFrame implements KeyListener {
+
+    private JTextArea textArea;
+    private boolean isAKeyPressed = false;
+    private boolean isWKeyPressed = false;
+    private boolean isSKeyPressed = false;
+    private boolean isDKeyPressed = false;
+
+    public KeyEventDemo() {
+        setTitle("Key Event Demo");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        add(textArea);
+        addKeyListener(this);
+        setFocusable(true); // フレームがフォーカスを受け取れるようにする
+        requestFocusInWindow(); // フレームにフォーカスを要求する
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // キーがタイプされたときの処理
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyChar()) {
+            case 'a':
+                isAKeyPressed = true;
+                break;
+            case 'w':
+            case ' ':
+                isWKeyPressed = true;
+                break;
+            case 's':
+                isSKeyPressed = true;
+                break;
+            case 'd':
+                isDKeyPressed = true;
+                break;
+        }
+
+        updateDirection();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        switch (e.getKeyChar()) {
+            case 'a':
+                isAKeyPressed = false;
+                break;
+            case 'w':
+            case ' ':
+                isWKeyPressed = false;
+                break;
+            case 's':
+                isSKeyPressed = false;
+                break;
+            case 'd':
+                isDKeyPressed = false;
+                break;
+        }
+    }
+
+    private void updateDirection() {
+        if (isWKeyPressed && isAKeyPressed) {
+            textArea.append("↖︎\n");
+        } else if (isWKeyPressed && isDKeyPressed) {
+            textArea.append("↗︎\n");
+        } else if (isSKeyPressed && isAKeyPressed) {
+            textArea.append("↙︎\n");
+        } else if (isSKeyPressed && isDKeyPressed) {
+            textArea.append("↘︎\n");
+        } else if (isWKeyPressed) {
+            textArea.append("↑\n");
+        } else if (isSKeyPressed) {
+            textArea.append("↓\n");
+        } else if (isAKeyPressed) {
+            textArea.append("←\n");
+        } else if (isDKeyPressed) {
+            textArea.append("→\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        KeyEventDemo frame = new KeyEventDemo();
+        frame.setVisible(true);
+    }
+}
---
+++ map.java
@@ -0,0 +1,29 @@
+// import java.util.ArrayList;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         // ArrayListの作成
+//         ArrayList<Hero> heroes = new ArrayList<>();
+
+//         // Heroのインスタンス化とArrayListへの追加
+//         heroes.add(new Hero("斎藤"));
+//         heroes.add(new Hero("鈴木"));
+
+//         // ArrayListから1人ずつ取り出して名前を表示
+//         for (Hero hero : heroes) {
+//             System.out.println("勇者の名前: " + hero.getName());
+//         }
+//     }
+// }
+
+// class Hero {
+//     private String name;
+
+//     public Hero(String name) {
+//         this.name = name;
+//     }
+
+//     public String getName() {
+//         return this.name;
+//     }
+// }
